f868576cc1a5f55b72e2a47efcdf84db91b20a6f
ardnew <andrew@ardnew.com>
2024-01-16 18:48:31 -0600

    unexport Field, add type converter AsField

--- option.go
+++ option.go
@@ -1,6 +1,6 @@
 package walk
 
 // Optional is a type with which Option arguments and methods can be applied.
-type Optional interface{ *Model | *Field }
+type Optional interface{ *Model | *field }
 
 type Option[O Optional] func(O) O
